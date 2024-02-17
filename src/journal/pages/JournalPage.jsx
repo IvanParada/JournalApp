c3131b61510ba8760aa46b69b9e9a49705005a6a
Iván Parada <ivan.parada2001@alumnos.ubiobio.cl>
2024-02-17 14:51:56 -0300

    noteview

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,12 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Elit incididunt id sunt aliqua anim mollit incididunt esse commodo aliqua pariatur nisi qui minim. Id dolor adipisicing amet non nostrud aliqua adipisicing. Ipsum elit consectetur cillum consequat sint aliqua velit occaecat velit laboris dolore. Aute incididunt velit laboris est minim. Nisi qui nostrud eu duis nulla minim id elit ipsum. Magna id irure dolor elit velit deserunt.</Typography>
+
+      <NothingSelectedView />
 
       {/* NothinSelected */}
       {/* NoteView */}
